875613d1dcde1de7bf400bc39eee3fb35253f5c7
VMDevelopèr <vicmr1994@gmail.com>
2017-12-28 18:17:59 +0100

    añadido Options Menu y su funcionalidad básica

--- app/src/main/java/application/android/vicinflames/disponibilidad_gplaces/Activities/MainActivity.java
+++ app/src/main/java/application/android/vicinflames/disponibilidad_gplaces/Activities/MainActivity.java
@@ -2,6 +2,9 @@ package application.android.vicinflames.disponibilidad_gplaces.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import application.android.vicinflames.disponibilidad_gplaces.R;
 
@@ -12,4 +15,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        switch( item.getItemId() ) {
+            case R.id.new_place:
+                Toast.makeText(this, "Item " + item.getItemId() + "elegido!" , Toast.LENGTH_SHORT).show();
+
+        }
+        return true;
+    }
+
+
 }
