8ed0912f9bf503b4172acc7d6b405f86ab68c6a9
VMDevelopèr <vicmr1994@gmail.com>
2017-12-30 17:46:09 +0100

    ( clases JAVA ) Añadidos fragments y activities - main/welcome, maps, y data ( de coords )

---
+++ app/src/main/java/application/android/vicinflames/disponibilidad_gplaces/Activities/DataActivity.java
@@ -0,0 +1,8 @@
+package application.android.vicinflames.disponibilidad_gplaces.Activities;
+
+/**
+ * Created by VicInFlames on 30/12/2017.
+ */
+
+public class DataActivity {
+}
---
+++ app/src/main/java/application/android/vicinflames/disponibilidad_gplaces/Fragments/DataFragment.java
@@ -0,0 +1,8 @@
+package application.android.vicinflames.disponibilidad_gplaces.Fragments;
+
+/**
+ * Created by VicInFlames on 30/12/2017.
+ */
+
+public class DataFragment {
+}
